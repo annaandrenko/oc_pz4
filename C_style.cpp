2aafe5982176fcd4fac2f62dcb56cb593343bea0
anyaa <anna.andrenko@gmail.com>
2026-03-26 18:21:49 +0200

    update

--- C_style.cpp
+++ C_style.cpp
@@ -1,38 +1,35 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <fstream>
+#include <cstdio>
 #include <iomanip>
 #include <cstring>
 
 using namespace std;
 
 /*                                    Завдання 1.
-1. Використовуючи введення - виведення в стилі С створити файл та записати в нього структуровані дані.
+1. Використовуючи введення - виведення в стилі С створити файл та записати в нього
+структуровані дані.
 2. Вивести створений файл на екран.
 3. Видалити з файлу дані відповідно до варіанта.
 4. Додати в файл дані відповідно до варіанта.
 5. Вивести змінений файл на екран.
-6. Використовуючи введення - виведення в стилі С + +створити файл та записати в нього структуровані дані.
-7. Вивести створений файл на екран.
-8. Видалити з файлу дані відповідно до варіанта.
-9. Додати в файл дані відповідно до варіанта.
-10. Вивести змінений файл на екран.
 
                                        Варіант 1
-Структура данных		
-Структура "Абитуриент":
--	фамилия, имя, отчество;
--	год рождения;
--	оценки вступительных экзаменов (3);
--	средний балл аттестата.	
+Структура данных
+Структура "Абитуриент": 
+фамилия, имя, отчество;
+год рождения;
+оценки вступительных экзаменов (3);
+средний балл аттестата.
 
 Удаление
 Удалить элемент с указанным номером.
-   
+
 Добавление
 Добавить K элементов в начало файла
-
 */
+
+
 
 
 struct Abiturient {
@@ -94,9 +91,9 @@ void printAbiturient(const Abiturient& a, int index) {
         << endl;
 }
 
-void createFile(const char* filename) {
-    ofstream f(filename, ios::binary);
-    if (!f) {
+void createFileC(const char* filename) {
+    FILE* f = fopen(filename, "wb");
+    if (f == NULL) {
         cout << "Помилка відкриття файлу для запису.\n";
         return;
     }
@@ -109,16 +106,21 @@ void createFile(const char* filename) {
     for (int i = 0; i < n; i++) {
         cout << "\nВведення даних абітурієнта " << i + 1 << ":\n";
         inputAbiturient(a);
-        f.write(reinterpret_cast<char*>(&a), sizeof(Abiturient));
+
+        if (fwrite(&a, sizeof(Abiturient), 1, f) != 1) {
+            cout << "Помилка запису у файл.\n";
+            fclose(f);
+            return;
+        }
     }
 
-    f.close();
+    fclose(f);
     cout << "\nФайл успішно створено.\n";
 }
 
-void showFile(const char* filename) {
-    ifstream f(filename, ios::binary);
-    if (!f) {
+void showFileC(const char* filename) {
+    FILE* f = fopen(filename, "rb");
+    if (f == NULL) {
         cout << "Помилка відкриття файлу для читання.\n";
         return;
     }
@@ -130,7 +132,7 @@ void showFile(const char* filename) {
     cout << "\nВміст файлу:\n";
     printHeader();
 
-    while (f.read(reinterpret_cast<char*>(&a), sizeof(Abiturient))) {
+    while (fread(&a, sizeof(Abiturient), 1, f) == 1) {
         printAbiturient(a, i++);
         empty = false;
     }
@@ -139,80 +141,12 @@ void showFile(const char* filename) {
         cout << "Файл порожній.\n";
     }
 
-    f.close();
+    fclose(f);
 }
 
-void deleteByNumber(const char* filename, int number) {
-    ifstream fin(filename, ios::binary);
-    ofstream fout("temp.dat", ios::binary);
-
-    if (!fin || !fout) {
-        cout << "Помилка відкриття файлу.\n";
-        return;
-    }
-
-    Abiturient a;
-    int i = 1;
-    bool found = false;
-
-    while (fin.read(reinterpret_cast<char*>(&a), sizeof(Abiturient))) {
-        if (i != number) {
-            fout.write(reinterpret_cast<char*>(&a), sizeof(Abiturient));
-        }
-        else {
-            found = true;
-        }
-        i++;
-    }
-
-    fin.close();
-    fout.close();
-
-    remove(filename);
-    rename("temp.dat", filename);
-
-    if (found)
-        cout << "Елемент №" << number << " успішно видалено.\n";
-    else
-        cout << "Елемент із таким номером не знайдено.\n";
-}
-
-void addToBeginning(const char* filename, int k) {
-    ifstream fin(filename, ios::binary);
-    ofstream fout("temp.dat", ios::binary);
-
-    if (!fout) {
-        cout << "Помилка відкриття тимчасового файлу.\n";
-        return;
-    }
-
-    Abiturient a;
-
-    cout << "\nВведення " << k << " нових елементів для додавання на початок:\n";
-    for (int i = 0; i < k; i++) {
-        cout << "\nНовий абітурієнт " << i + 1 << ":\n";
-        inputAbiturient(a);
-        fout.write(reinterpret_cast<char*>(&a), sizeof(Abiturient));
-    }
-
-    if (fin) {
-        while (fin.read(reinterpret_cast<char*>(&a), sizeof(Abiturient))) {
-            fout.write(reinterpret_cast<char*>(&a), sizeof(Abiturient));
-        }
-        fin.close();
-    }
-
-    fout.close();
-
-    remove(filename);
-    rename("temp.dat", filename);
-
-    cout << "\nНові елементи успішно додано на початок файлу.\n";
-}
-
-void searchBySurname(const char* filename, const char* surname) {
-    ifstream f(filename, ios::binary);
-    if (!f) {
+void searchBySurnameC(const char* filename, const char* surname) {
+    FILE* f = fopen(filename, "rb");
+    if (f == NULL) {
         cout << "Помилка відкриття файлу для пошуку.\n";
         return;
     }
@@ -224,7 +158,7 @@ void searchBySurname(const char* filename, const char* surname) {
     cout << "\nРезультат пошуку за прізвищем \"" << surname << "\":\n";
     printHeader();
 
-    while (f.read(reinterpret_cast<char*>(&a), sizeof(Abiturient))) {
+    while (fread(&a, sizeof(Abiturient), 1, f) == 1) {
         if (strcmp(a.prizvyshche, surname) == 0) {
             printAbiturient(a, i);
             found = true;
@@ -236,39 +170,111 @@ void searchBySurname(const char* filename, const char* surname) {
         cout << "Записів із таким прізвищем не знайдено.\n";
     }
 
-    f.close();
+    fclose(f);
+}
+
+void deleteByNumberC(const char* filename, int number) {
+    FILE* f = fopen(filename, "rb");
+    FILE* temp = fopen("temp_c.dat", "wb");
+
+    if (f == NULL || temp == NULL) {
+        cout << "Помилка відкриття файлу.\n";
+        if (f) fclose(f);
+        if (temp) fclose(temp);
+        return;
+    }
+
+    Abiturient a;
+    int i = 1;
+    bool found = false;
+
+    while (fread(&a, sizeof(Abiturient), 1, f) == 1) {
+        if (i != number) {
+            fwrite(&a, sizeof(Abiturient), 1, temp);
+        }
+        else {
+            found = true;
+        }
+        i++;
+    }
+
+    fclose(f);
+    fclose(temp);
+
+    remove(filename);
+    rename("temp_c.dat", filename);
+
+    if (found)
+        cout << "Елемент №" << number << " успішно видалено.\n";
+    else
+        cout << "Елемент із таким номером не знайдено.\n";
+}
+
+void addToBeginningC(const char* filename, int k) {
+    FILE* f = fopen(filename, "rb");
+    FILE* temp = fopen("temp_c.dat", "wb");
+
+    if (temp == NULL) {
+        cout << "Помилка відкриття тимчасового файлу.\n";
+        if (f) fclose(f);
+        return;
+    }
+
+    Abiturient a;
+
+    cout << "\nВведення " << k << " нових елементів для додавання на початок:\n";
+    for (int i = 0; i < k; i++) {
+        cout << "\nНовий абітурієнт " << i + 1 << ":\n";
+        inputAbiturient(a);
+        fwrite(&a, sizeof(Abiturient), 1, temp);
+    }
+
+    if (f != NULL) {
+        while (fread(&a, sizeof(Abiturient), 1, f) == 1) {
+            fwrite(&a, sizeof(Abiturient), 1, temp);
+        }
+        fclose(f);
+    }
+
+    fclose(temp);
+
+    remove(filename);
+    rename("temp_c.dat", filename);
+
+    cout << "\nНові елементи успішно додано на початок файлу.\n";
 }
 
 int main() {
     system("chcp 1251 > nul");
 
-    const char* filename = "abiturient.dat";
+    const char* filename = "abiturient_c.dat";
     int deleteNumber, k;
     char surname[30];
 
+    cout << "========= РОБОТА З ФАЙЛОМ У СТИЛІ C =========\n";
 
-    createFile(filename);
-    showFile(filename);
+    createFileC(filename);
+    showFileC(filename);
 
     cout << "\nВведіть прізвище для пошуку: ";
     cin >> surname;
-    searchBySurname(filename, surname);
+    searchBySurnameC(filename, surname);
 
     cout << "\nВведіть номер елемента для видалення: ";
     cin >> deleteNumber;
-    deleteByNumber(filename, deleteNumber);
+    deleteByNumberC(filename, deleteNumber);
 
     cout << "Введіть кількість K елементів для додавання на початок: ";
     cin >> k;
-    addToBeginning(filename, k);
+    addToBeginningC(filename, k);
 
     cout << "\nЗмінений файл:\n";
-    showFile(filename);
+    showFileC(filename);
 
     cout << "\nПовторний пошук у зміненому файлі.\n";
     cout << "Введіть прізвище для пошуку: ";
     cin >> surname;
-    searchBySurname(filename, surname);
+    searchBySurnameC(filename, surname);
 
     return 0;
 }
